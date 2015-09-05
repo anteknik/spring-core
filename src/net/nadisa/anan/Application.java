e5388a8c522bec99836d546250ea9278b578c627
Anton Purwanto <anton.takaful@gmail.com>
2015-09-05 10:07:32 +0800

    spring custom-event

--- src/net/nadisa/anan/Application.java
+++ src/net/nadisa/anan/Application.java
@@ -1,20 +1,19 @@
 package net.nadisa.anan;
 
-import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import net.nadisa.anan.event.publisher.UserDefinedEventPublisher;
 
 public class Application {
 	public static void main(String[] args) {
 
-		//ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 		//AbstractApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-		ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-		context.refresh();
-		context.start();
+		//ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 		Greetings greetings = (Greetings) context.getBean("greeting");
 		System.out.println(greetings.getMessage());
-		//context.registerShutdownHook();
-		context.stop();
-		context.close();
+		UserDefinedEventPublisher publisher = (UserDefinedEventPublisher) context.getBean("eventPublisher");
+		publisher.publish();
 	}
 }
