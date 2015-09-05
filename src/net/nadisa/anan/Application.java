4adb1fabfdab5de2c84fb2f05d73c285abd21f29
Anton Purwanto <anton.takaful@gmail.com>
2015-09-05 09:26:38 +0800

    spring applicationcontext-classpath

--- src/net/nadisa/anan/Application.java
+++ src/net/nadisa/anan/Application.java
@@ -1,13 +1,14 @@
 package net.nadisa.anan;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Application {
 	public static void main(String[] args) {
 
-		ApplicationContext factory = new FileSystemXmlApplicationContext("beans.xml");
-		Greetings greetings = (Greetings) factory.getBean("greeting");
+		//ApplicationContext factory = new FileSystemXmlApplicationContext("beans.xml");
+		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		Greetings greetings = (Greetings) context.getBean("greeting");
 		System.out.println(greetings.getMessage());
 	}
 }
