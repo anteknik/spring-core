ad0ea08491f974b41ed7397dc85d09219313e048
Anton Purwanto <anton.takaful@gmail.com>
2015-09-05 10:17:01 +0800

    spring scope

--- src/net/nadisa/anan/Application.java
+++ src/net/nadisa/anan/Application.java
@@ -1,19 +1,20 @@
 package net.nadisa.anan;
 
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import net.nadisa.anan.event.publisher.UserDefinedEventPublisher;
 
 public class Application {
 	public static void main(String[] args) {
 
-		ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-		//AbstractApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		//ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 		//ConfigurableApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
 		Greetings greetings = (Greetings) context.getBean("greeting");
 		System.out.println(greetings.getMessage());
-		UserDefinedEventPublisher publisher = (UserDefinedEventPublisher) context.getBean("eventPublisher");
-		publisher.publish();
+
+		Greetings greetings2 = (Greetings) context.getBean("greeting");
+		greetings2.setMessage("Hello Spring 2");
+		System.out.println(greetings2 + " : " + greetings.getMessage());
+		context.registerShutdownHook();
 	}
 }
