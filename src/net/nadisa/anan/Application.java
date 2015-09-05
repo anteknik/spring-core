dfead9f8ae68f4726f7fdba9689c7955f8de6fde
Anton Purwanto <anton.takaful@gmail.com>
2015-09-05 11:19:11 +0800

    spring annotation multiple dependent

--- src/net/nadisa/anan/Application.java
+++ src/net/nadisa/anan/Application.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import net.nadisa.anan.config.BeanConfig;
 import net.nadisa.anan.config.BeanConfig2;
 import net.nadisa.anan.entities.Greetings;
+import net.nadisa.anan.entities.Greetings2;
+import net.nadisa.anan.entities.Greetings3;
 
 public class Application {
 	public static void main(String[] args) {
@@ -16,9 +18,12 @@ public class Application {
 		Greetings greetings = (Greetings) context.getBean(Greetings.class);
 		System.out.println(greetings +" :"+greetings.getMessage());
 
-		Greetings greetings2 = (Greetings) context.getBean(Greetings.class);
-		greetings2.setMessage("Hello Spring 2");
+		Greetings2 greetings2 = (Greetings2) context.getBean(Greetings2.class);
 		System.out.println(greetings2 + " : " + greetings2.getMessage());
+		
+		Greetings3 greetings3 = (Greetings3) context.getBean(Greetings3.class);
+		System.out.println(
+				greetings3 + " : " + greetings3.getGreetings() + " : " + greetings3.getGreetings().getMessage());
 		
 	}
 }
---
+++ src/net/nadisa/anan/entities/Greetings3.java
@@ -0,0 +1,14 @@
+package net.nadisa.anan.entities;;
+
+public class Greetings3 {
+	private Greetings greetings;
+
+	public Greetings getGreetings() {
+		return greetings;
+	}
+
+	public void setGreetings(Greetings greetings) {
+		this.greetings = greetings;
+	}
+
+}
